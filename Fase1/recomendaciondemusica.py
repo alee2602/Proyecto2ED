0aab6b1e08b665e9b5a0f0112312860633b68855
Mónica Salvatierra <104535119+alee2602@users.noreply.github.com>
2023-05-03 19:45:35 -0600

    Version 1.1
    Crear las canciones en el database, además de funciones para preguntarle al usuario por su estado de ánimo y géneros musicales preferidos

--- Fase1/recomendaciondemusica.py
+++ Fase1/recomendaciondemusica.py
@@ -11,6 +11,30 @@ from neo4j import SongDataBase
 url=""  #Dirección de la base de datos
 username="" #Usuario para acceder a la base de datos
 password="" #Contraseña para acceder a la base de datos
-
 driver= SongDataBase.driver(url, auth=(username, password))
+
+#Crear las canciones en la base de datos
+
+def crear_canciones_db(db):
+    for cancion in canciones:
+        crear_query= f"CREATE (:Canción {{Nombre: '{cancion.name}', Género: '{cancion.genre}', Artista: '{cancion.artist}', Emoción Transmitida: '{cancion.emotion}'}})"
+        with db.session() as session:
+            session.run(crear_query)
+
+#Pedir al usuario su estado de ánimo actual
+
+def getemocion():
+
+    emocion="" #Estado de ánimo actual
+
+    return emocion
+
+#Pedir al usuario sus géneros preferidos
+
+def getgenerospreferidos():
+
+    generospreferidos="" #Géneros preferidos
+
+    return generospreferidos
+
 
