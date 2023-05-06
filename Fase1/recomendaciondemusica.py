86cb4ff1de7ac4545f5271e7288dcabd7d4227da
Mónica Salvatierra <104535119+alee2602@users.noreply.github.com>
2023-05-05 18:40:41 -0600

    Version 1.3

--- Fase1/recomendaciondemusica.py
+++ Fase1/recomendaciondemusica.py
@@ -4,18 +4,20 @@
 #Derek Arreaga, Giovanni Santos, César López, Isabella Miralles y Mónica Salvatierra
 #Algoritmo de Floyd para recomendar música en base al estado de ánimo y géneros musicales preferidos del usuario
 
-from neo4j import SongDataBase
+from neo4j import GraphDataBase
 
 #Crear la conexión a la base de datos
 
-url=""  #Dirección de la base de datos
-username="" #Usuario para acceder a la base de datos
-password="" #Contraseña para acceder a la base de datos
-driver= SongDataBase.driver(url, auth=(username, password))
+def connectneo4j():
+    url=""  #Dirección de la base de datos
+    username="" #Usuario para acceder a la base de datos
+    password="" #Contraseña para acceder a la base de datos
+    driver= GraphDataBase.driver(url, auth=(username, password))
+    return driver
 
 #Crear las canciones en la base de datos
 
-def crear_canciones_db(db):
+def crear_canciones_db(db,canciones):
     for cancion in canciones:
         crear_query= f"CREATE (:Canción {{Nombre: '{cancion.name}', Género: '{cancion.genre}', Artista: '{cancion.artist}', Emoción Transmitida: '{cancion.emotion}'}})"
         with db.session() as session:
@@ -23,26 +25,54 @@ def crear_canciones_db(db):
 
 #Pedir al usuario su estado de ánimo actual
 
-def getemocion():
+def getemocion(db):
 
-    emocion = input("Ingrese su estado de ánimo actual: ") # Pedir al usuario su estado de ánimo actual
-    guardar_emocion_query = f"CREATE (:Emoción {{Nombre: '{emocion}'}})" # Crear un nodo para la emoción ingresada por el usuario
-    with driver.session() as session:
-        session.run(guardar_emocion_query)
-    return emocion
+    emocion = input("Ingrese su estado de ánimo actual: ") #Emoción Actual
+    # Buscar las canciones que corresponden a la emoción ingresada y la emoción que transmite.
+    emocion_query = f"MATCH (c:Canción) WHERE c.Emoción Transmitida = '{emocion}' RETURN c"
+    with db.session() as session:
+        resultado = session.run(emocion_query)
+        canciones = [record["c"] for record in resultado]
+    return emocion, canciones
 
 #Pedir al usuario sus géneros preferidos
 
-def getgenerospreferidos():
+def getgenerospreferidos(db):
     
     generospreferidos=input("Ingrese sus géneros preferidos separados por comas: ") #Géneros preferidos
 
-    #Guardar los géneros preferidos en la base de datos
-    with driver.session() as session:
+    #Guarda los géneros preferidos en la base de datos, haciendo relación con los que ya están establecidos en la db.
+    with db.session() as session:
         for genero in generospreferidos.split(","):
-            crear_query= f"CREATE (:Género {{Nombre: '{genero.strip()}'}})"
-            session.run(crear_query)
+            relacion_query= f"MATCH (g:Género {{Nombre: '{genero.strip()}'}}) CREATE (u)-[:GUSTA]->(g)"
+            session.run(relacion_query)
 
     return generospreferidos
 
+#Ejecuta el funcionamiento del sistema mediante el algoritmo de Floyd
+def ejecutaralgoritmo(db, emocion, generospreferidos):
 
+    recomendacion_query= f"MATCH (c:Canción)-[:DE]->(e:Emoción {{Nombre: '{emocion}'}}) WHERE c.Género IN {generospreferidos} RETURN c"
+
+    #Ejecutar la consulta 
+    with db.session() as sesion:
+        result= sesion.run(recomendacion_query)
+        recomendaciones= [record["c"] for record in result]
+
+    return recomendaciones
+
+#Muestra las recomendaciones de las canciones
+def mostrar_recomendaciones(recomendaciones):
+
+    print("Canciones recomendadas:")
+    for cancion in recomendaciones:
+        print(cancion["Nombre"] + " - " + cancion["Artista"])
+
+#Manera en la que funciona el algoritmo
+def main():
+    driver = connectneo4j()
+    db = GraphDataBase(driver)
+    emocion= getemocion()
+    generospreferidos=getgenerospreferidos()
+    recomendaciones= ejecutaralgoritmo(emocion, generospreferidos)
+    mostrar_recomendaciones(recomendaciones)
